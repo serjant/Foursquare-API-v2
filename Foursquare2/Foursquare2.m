71a3044b0668698f4c5a51d79bb16d535c485083
David Baum <serjantik@gmail.com>
2013-11-28 17:11:20 +0200

    Fixed appending boolean URL parameters

--- Foursquare2/Foursquare2.m
+++ Foursquare2/Foursquare2.m
@@ -535,7 +535,7 @@ static NSMutableDictionary *attributes;
                                         price:(NSString *)price
                                      callback:(Foursquare2Callback)callback {
 	NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-    if (!near || !near.length || (!latitude && !longitude)) {
+    if ((!near) || (near && !near.length) || (!latitude && !longitude)) {
         NSAssert(NO, @"Foursqure2 venueExploreRecommendedNearByLatitude: near or ll are required parameters.");
     }
 	if (latitude && longitude) {
@@ -569,10 +569,10 @@ static NSMutableDictionary *attributes;
         dic[@"novelty"] = novelty;
     }
     if (openNow) {
-        dic[@"openNow"] = @(openNow);
+        dic[@"openNow"] = @(openNow).stringValue;
     }
     if (sortByDistance) {
-        dic[@"sortByDistance"] = @(sortByDistance);
+        dic[@"sortByDistance"] = @(sortByDistance).stringValue;
     }
     if (price) {
         dic[@"price"] = price;
